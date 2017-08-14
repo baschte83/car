22afb7a7aded13f6e80b333a43ac9fdf5af2ae47
Sebastian Baumann <bauman21@hm.edu>
2017-08-14 10:18:25 +0200

    Updating .gitignore file. Translating all comments into english.

--- src/Car.java
+++ src/Car.java
@@ -1,32 +1,48 @@
 /**
- * Organisation: Hochschule Muenchen, Fakultaet 07
- * Project: Praktikum Softwareentwicklung 1 Frau Prof. Dr. Hammerschall, WS 2016/2017
- * Author: Sebastian Baumann, bauman21@hm.edu
+ * Organisation: University of applied sciences Munich, faculty 07
+ * Project: lab to class software developement 1 Mrs. Prof. Dr. Hammerschall, WS 2016/2017
+ * Author: Sebastian Baumann, sebastian_baumann@gmx.net
  * Study group: IF3A
  * Date: 19. November 2016
- * Purpose: Loesung Praktikumseinheit 04: Car, Main-Test-Methode von Frau Prof. Dr. Hammerschall
- * Software: Oracle Java SE JDK 1.8.0_101, Windows 10 x86_64
- * Hardware: Intel Core i5-6300 @ 2.40 GHz 2.50 GHz, 2 Cores, 4096 MB RAM
+ * Purpose: solution to lab Car
  */
+class Car {
 
-public class Car {
-
-    // Konstanten -----------------------------------------------------
-    // Konstante fuer 100 km
+    /**
+     * Constant value for 100 km.
+     */
     private static final double A_HUNDRET_KILOMETERS = 100.0;
 
-    // Objektvariablen ------------------------------------------------
-    private final String type;          // final, da sich der Autotyp nicht mehr veraendern wird
-    private final double capacity;      // final, da sich das Tankvolumen nicht mehr aendern wird
-    private final double consumption;   // final, da sich der Verbrauch in unserem Beispiel nicht mehr aendern wird
-    private double fuel = 0;            // nicht final, da sich der Fuellstand aendern wird
-    private int kilometers = 0;         // nicht final, da sich der Kilometerstand aendern wird
-
-    // Konstruktor ----------------------------------------------------
     /**
-     * Hier wird davon ausgegangen, dass der Benutzer die Car-Klasse immer
-     * mit drei Parametern erzeugt, also kein Konstruktor mit zwei oder weniger
-     * uebergebenen Parametern programmiert werden muss.
+     * Private final variable to store the type of this car object.
+     */
+    private final String type;
+
+    /**
+     * Private final variable to store the tank capacity of this car object.
+     */
+    private final double capacity;
+
+    /**
+     * Private final variable to store the gasoline consumption of this car object.
+     */
+    private final double consumption;
+
+    /**
+     * Private variable to store the actual amount of gasoline in the tank of this car object.
+     */
+    private double fuel = 0;
+
+    /**
+     * Private variable to store the actual driven distance of this car object.
+     */
+    private int kilometers = 0;
+
+    /**
+     * Constructor of an object of this class.
+     * @param carType - type of this car object.
+     * @param tankCapacity - capacity of the tank of this car object.
+     * @param carConsumption - consumption of this car object.
      */
     Car(String carType, double tankCapacity, double carConsumption) {
 
@@ -37,15 +53,17 @@ public class Car {
     }
 
 
-    // Methoden -------------------------------------------------------
+    /**
+     * Method to fill a certain amount of gasoline into the tank of this car object.
+     * @param amount - amount of gasoline to fill in the tank.
+     */
     void fill(int amount) {
 
         this.fuel += amount;
 
-        /**
-         * Falls der Tankfuellstand groesser oder gleich dem Tankvolumen ist,
-         * soll der Tankfuellstand genau dem Tankvolumen entsprechen. Der
-         * ueberschuessige Rest wird "weggeschuettet".
+        /*
+         * If 'amount' is greater than the the amount of free space in the tank,
+         * than the spare amount of gasoline is tipped away.
          */
         if (this.fuel >= this.capacity) {
 
@@ -54,22 +72,23 @@ public class Car {
         }
     }
 
+    /**
+     * Method to drive the car for a distance of 'km' kilometers.
+     * @param km - the distance this car object has to drive.
+     */
     void drive(int km) {
 
-        /**
-         * Zuerst stelle ich fest, wie viel Treibstoff beim fahren der
-         * geforderten Kilometer "km" verbraucht werden wuerde. Das wird in
-         * fuelUsed gespeichert.
+        /*
+         * 'fuelUsed' stores the actual amount of gasoline this car object needs to
+         * drive the demanded 'km' distance.
          */
         double fuelUsed = (this.consumption / A_HUNDRET_KILOMETERS) * km;
 
-        /**
-         * Sollte die Menge an Benzin, die ich braeuchte, um die geforderten
-         * Kilometer "km" zu fahren, nicht in meinem Tank sein, so kann ich nur so
-         * viele Kilometer fahren, wie ich mit meinem Tankinhalt eben schaffe. Diese
-         * Strecke wird im if-Teil der if-else-Verzweigung berechnet und dem Kilometer-
-         * Stand "kilometers" hinzugerechnet. Dabei wird ja der Tank leer gefahren. Deshalb
-         * wird der Tankfuellstand "fuel" auf 0 gesetzt.
+        /*
+         * If 'fuelUsed' is greater than the amount of gasoline this car object has (this.fuel),
+         * this car object only drives the distance it can go with the gasoline in 'this.fuel'.
+         * The left over distance from 'km' is tipped away. After the car has driven its distance
+         * the variable 'fuel' is updated.
          */
         if (fuelUsed >= this.fuel) {
 
@@ -77,14 +96,6 @@ public class Car {
             this.fuel = 0;
 
         }
-        /**
-         * Im else-Teil der if-else-Verzweigung bin ich jetzt sicher, dass ich noch genug
-         * Benzin im Tank habe, um die geforderte Strecke "km" zu fahren. Also rechne ich
-         * zu meinem Kilometerstand "kilometers" die geforderte Strecke "km" hinzu und
-         * ziehe von meinem Tankfuellstand "fuel" die verbrauchte Menge Treibstoff ab.
-         * Der Tankinhalt kann dabei nicht negativ werden, da "fuelUsed" nicht groesser als
-         * mein Tankinhalt "fuel" sein kann. Das wird im if-Teil abgefangen..
-         */
         else {
 
             this.kilometers += km;
@@ -93,7 +104,10 @@ public class Car {
         }
     }
 
-    // Mit String.format formatierte Ausgabe.
+    /**
+     * This mehtod generates a String representation of this car object.
+     * @return - returns a string with all values stored in this car object.
+     */
     public String toString() {
 
         return String.format("%s(%.0f,%d)", type, fuel, kilometers);
--- src/CarMain.java
+++ src/CarMain.java
@@ -1,59 +1,54 @@
 /**
- * Organisation: Hochschule Muenchen, Fakultaet 07
- * Project: Praktikum Softwareentwicklung 1 Frau Prof. Dr. Hammerschall, WS 2016/2017
- * Author: Sebastian Baumann, bauman21@hm.edu
+ * Organisation: University of applied sciences Munich, faculty 07
+ * Project: lab to class software developement 1 Mrs. Prof. Dr. Hammerschall, WS 2016/2017
+ * Author: Sebastian Baumann, sebastian_baumann@gmx.net
  * Study group: IF3A
  * Date: 19. November 2016
- * Purpose: Loesung Praktikumseinheit 04: CarMain, Main-Test-Methode von Frau Prof. Dr. Hammerschall
- * Software: Oracle Java SE JDK 1.8.0_101, Windows 10 x86_64
- * Hardware: Intel Core i5-6300 @ 2.40 GHz 2.50 GHz, 2 Cores, 4096 MB RAM
- */
-
-/**
- * Testklasse Auto
- * @author Ulrike Hammerschall
+ * Purpose: solution to lab Car
  */
 public class CarMain {
 
     /**
-     * main-Methode mit Testprozedur. Erwartet keine Eingabewerte.
-     * @param args
+     * Main method to test the functionality of class car.
+     * @param args - variable number of arguments from the terminal.
      */
     public static void main(String[] args) {
 
-        // Initialisierung der Testautos
+        // Initializes two new cars.
         Car fox = new Car("Fox", 35, 4);
         Car hummer = new Car("Hummer", 100, 25);
 
-        // Tanken 30 Liter
+        // Both cars gas up 30 liters of gasoline.
         fox.fill(30);
         hummer.fill(30);
-        System.out.println(fox.toString());    // Fox(30,0)
-        System.out.println(hummer.toString()); // Hummer(30,0)
 
-        // 100 km weit fahren
+        // Printing the states of both cars to the screen.
+        System.out.println("Should be Fox(30,0). Is: " + fox.toString());
+        System.out.println("Should be Hummer(30,0). Is: " + hummer.toString());
+
+        // Both cars have to drive 100 kilometers.
         fox.drive(100);
-        System.out.println(fox.toString());    // Fox(26,100)
+        System.out.println("Should be Fox(26,100). Is: " + fox.toString());
         hummer.drive(100);
-        System.out.println(hummer.toString()); // Hummer(5,100)
+        System.out.println("Should be Hummer(5,100). Is: " + hummer.toString());
 
-        // 100 km weit fahren
+        // Both cars have to drive another 100 kilometers.
         fox.drive(100);
         hummer.drive(100);
-        System.out.println(fox.toString());    // Fox(22,200)
-        System.out.println(hummer.toString()); // Hummer(0,120)
+        System.out.println("Should be Fox(22,200). Is: " + fox.toString());
+        System.out.println("Should be Hummer(0,120). Is: " + hummer.toString());
 
-        // Tanken 30 Liter
+        // Both cars gas up another 30 liters of gasoline.
         fox.fill(30);
         hummer.fill(30);
-        System.out.println(fox.toString());    // Fox(35,200)
-        System.out.println(hummer.toString()); // Hummer(30,120)
+        System.out.println("Should be Fox(35,200). Is: " + fox.toString());
+        System.out.println("Should be Hummer(30,120). Is: " + hummer.toString());
 
-        // 200 km weit fahren
+        // Both cars have to drive 200 kilometers.
         fox.drive(200);
         hummer.drive(200);
-        System.out.println(fox.toString());    // Fox(27,400)
-        System.out.println(hummer.toString()); // Hummer(0,240)
+        System.out.println("Should be Fox(27,400). Is: " + fox.toString());
+        System.out.println("Should be Hummer(0,240). Is: " + hummer.toString());
     }
 
 }
